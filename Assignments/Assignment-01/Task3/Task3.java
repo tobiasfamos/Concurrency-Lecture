240c81456ffa6059a1c870ed10e50bf2ca7b0d64
Tobias Famos <tobias.famos@outlook.com>
2023-03-09 11:19:34 +0100

    Add Task3

---
+++ Assignments/Assignment-01/Task3/Task3.java
@@ -0,0 +1,10 @@
+/*Consider the classical producer-consumer problem: a group of P producer threads and a group of C consumer threads share a bounded circular buffer of size N. If the buffer is not full,
+* producers are allowed to add elements; if the buffer is not empty, consumers can consume elements. Write a program that can correctly coordinate the producers and consumers and 
+* their depositing and retrieving activities. For simplicity we assume that we have the same number T of producers and consumer threads. T and N are program arguments.
+*/
+public class Task3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
